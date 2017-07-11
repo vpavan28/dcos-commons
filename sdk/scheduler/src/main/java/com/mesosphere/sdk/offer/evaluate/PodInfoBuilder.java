fae8e349686c48b01b591d7f3f4cb1f852fcc011
Dinesh Israni <disrani@portworx.com>
2017-07-11 12:34:00 -0700

    Favor creating volumes locally for pxd driver

--- sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/PodInfoBuilder.java
+++ sdk/scheduler/src/main/java/com/mesosphere/sdk/offer/evaluate/PodInfoBuilder.java
@@ -642,6 +642,14 @@ public class PodInfoBuilder {
                         .setValue(Integer.toString(sizeGB))
                         .build());
 
+                if (dockerVolume.getDriverName().equals("pxd")) {
+                    // Favor creating volumes on the local node
+                    paramsList.add(Protos.Parameter.newBuilder()
+                            .setKey("nodes")
+                            .setValue("LocalNode")
+                            .build());
+                }
+
                 driverOptions.addAllParameter(paramsList);
                 driverOptions.build();
 
