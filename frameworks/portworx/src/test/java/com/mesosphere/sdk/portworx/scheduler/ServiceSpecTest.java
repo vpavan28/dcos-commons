5c49bb2f300eeb509594fd29310e923049c0e226
Dinesh Israni <disrani@portworx.com>
2017-07-19 15:08:17 -0700

    Updates to Portworx framework after rebase

--- frameworks/portworx/src/test/java/com/mesosphere/sdk/portworx/scheduler/ServiceSpecTest.java
+++ frameworks/portworx/src/test/java/com/mesosphere/sdk/portworx/scheduler/ServiceSpecTest.java
@@ -1,42 +1,41 @@
 package com.mesosphere.sdk.portworx.scheduler;
 
 import com.mesosphere.sdk.testing.BaseServiceSpecTest;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ServiceSpecTest extends BaseServiceSpecTest {
 
-    @BeforeClass
-    public static void beforeAll() {
-        ENV_VARS.set("EXECUTOR_URI", "");
-        ENV_VARS.set("LIBMESOS_URI", "");
-        ENV_VARS.set("PORT_API", "8080");
-        ENV_VARS.set("FRAMEWORK_NAME", "portworx");
+    public ServiceSpecTest() {
+        super(
+                "EXECUTOR_URI", "",
+                "LIBMESOS_URI", "",
+                "PORT_API", "8080",
+                "FRAMEWORK_NAME", "portworx",
 
-        ENV_VARS.set("NODE_COUNT", "3");
-        ENV_VARS.set("NODE_CPUS", "0.1");
-        ENV_VARS.set("NODE_MEM", "512");
+                "NODE_COUNT", "3",
+                "NODE_CPUS", "0.1",
+                "NODE_MEM", "512",
 
-        ENV_VARS.set("ETCD_COUNT", "3");
-        ENV_VARS.set("ETCD_CPUS", "0.1");
-        ENV_VARS.set("ETCD_MEM", "512");
-        ENV_VARS.set("ETCD_DISK_TYPE", "ROOT");
-        ENV_VARS.set("ETCD_DISK_SIZE", "5120");
-        ENV_VARS.set("ETCD_IMAGE", "mesosphere/etcd-mesos:latest");
-        ENV_VARS.set("ETCD_NODE_ADVERTISE_PORT", "1026");
-        ENV_VARS.set("ETCD_NODE_PEER_PORT", "1027");
-        ENV_VARS.set("ETCD_PROXY_ADVERTISE_PORT", "2379");
+                "ETCD_COUNT", "3",
+                "ETCD_CPUS", "0.1",
+                "ETCD_MEM", "512",
+                "ETCD_DISK_TYPE", "ROOT",
+                "ETCD_DISK_SIZE", "5120",
+                "ETCD_IMAGE", "mesosphere/etcd-mesos:latest",
+                "ETCD_NODE_ADVERTISE_PORT", "1026",
+                "ETCD_NODE_PEER_PORT", "1027",
+                "ETCD_PROXY_ADVERTISE_PORT", "2379",
 
-        ENV_VARS.set("INFLUXDB_CPUS", "0.1");
-        ENV_VARS.set("INFLUXDB_MEM", "512");
-        ENV_VARS.set("INFLUXDB_DISK_TYPE", "ROOT");
-        ENV_VARS.set("INFLUXDB_DISK_SIZE", "1024");
-        ENV_VARS.set("INFLUXDB_IMAGE", "influxdb:latest");
+                "INFLUXDB_CPUS", "0.1",
+                "INFLUXDB_MEM", "512",
+                "INFLUXDB_DISK_TYPE", "ROOT",
+                "INFLUXDB_DISK_SIZE", "1024",
+                "INFLUXDB_IMAGE", "influxdb:latest",
 
-        ENV_VARS.set("LIGHTHOUSE_CPUS", "0.1");
-        ENV_VARS.set("LIGHTHOUSE_MEM", "512");
-        ENV_VARS.set("LIGHTHOUSE_WEBUI_PORT", "8085");
-        ENV_VARS.set("LIGHTHOUSE_IMAGE", "portworx/px-lighthouse:latest");
+                "LIGHTHOUSE_CPUS", "0.1",
+                "LIGHTHOUSE_MEM", "512",
+                "LIGHTHOUSE_WEBUI_PORT", "8085",
+                "LIGHTHOUSE_IMAGE", "portworx/px-lighthouse:latest");
     }
 
     @Test
